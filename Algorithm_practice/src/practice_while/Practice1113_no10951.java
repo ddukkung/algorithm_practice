0ec94c7f32c62cec8237628f691f67c36aba5c0b
Jaehee Bae <gjznn3657@gmail.com>
2021-11-13 19:03:22 +0900

    while문 10951번 문제

---
+++ Algorithm_practice/src/practice_while/Practice1113_no10951.java
@@ -0,0 +1,20 @@
+package practice_while;
+
+import java.util.*;
+
+public class Practice1113_no10951 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNext()) {
+			int num1 = sc.nextInt();
+			int num2 = sc.nextInt();
+			
+			System.out.println(num1 + num2);
+		}
+
+	}
+
+}
