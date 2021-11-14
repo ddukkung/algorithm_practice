670bb27333624df030dd09bbf7c3388f77b19a03
Jaehee Bae <gjznn3657@gmail.com>
2021-11-14 19:51:04 +0900

    문자열 11645번

---
+++ Algorithm_practice/src/practice_string/Practice1114_no11654.java
@@ -0,0 +1,17 @@
+package practice_string;
+
+import java.util.*;
+
+public class Practice1114_no11654 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int ch = sc.next().charAt(0);
+		
+		System.out.println(ch);
+
+	}
+
+}
