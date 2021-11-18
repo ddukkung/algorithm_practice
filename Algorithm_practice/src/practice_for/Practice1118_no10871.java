69f68f0a4d25022ba36069e25d6fc0d8ddabe018
Jaehee Bae <gjznn3657@gmail.com>
2021-11-18 21:24:04 +0900

    for문 10871번

---
+++ Algorithm_practice/src/practice_for/Practice1118_no10871.java
@@ -0,0 +1,28 @@
+package practice_for;
+
+import java.util.*;
+
+public class Practice1118_no10871 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+		
+		int[] arr = new int[n];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] < x) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+
+	}
+
+}
