e59d01361e0fb4928c72a9bb3e39f50e67b9b1c8
Jaehee Bae <gjznn3657@gmail.com>
2021-11-24 22:43:32 +0900

    if문 2753번

---
+++ Algorithm_practice/src/Practice1124_no2753.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Practice1124_no2753 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		int year = sc.nextInt();
+		
+		if(year % 4 == 0) {
+			if(year % 400 == 0) {
+				System.out.println("1");
+			} else if(year % 100 == 0 ) {
+				System.out.println("0");
+			} else {
+				System.out.println("1");
+			}
+		} else {
+			System.out.println("0");
+		}
+
+	}
+
+}
